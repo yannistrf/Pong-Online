3972abf44d06bb30eea17eaa5f8f86e1a3282bd0
Yannis Triantafyllopoulos <yannistrf@gmail.com>
2024-10-19 23:17:10 +0300

    Implement initial tcp connection, then exchange of chosen udp ports over the tcp socket

--- client/main.cpp
+++ client/main.cpp
@@ -1,18 +1,147 @@
-#include <SFML/Graphics.hpp>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+#include <sys/socket.h>
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+#define SERVER_LISTEN_PORT 8080
+#define print_sock_err(str)   perror(str); perror(strerror(errno)); perror("\n");
 
-        window.clear();
-        window.display();
+typedef struct
+{
+    int recv_fd;
+    uint16_t recv_port;
+    struct sockaddr_in cliaddr;
+
+    int send_fd;
+    uint16_t send_port;
+    struct sockaddr_in servaddr;
+} UdpInfo;
+
+class Client
+{
+    public:
+        Client();
+        ~Client();
+        bool CreateTcpConnectionWithServer(int *tcp_fd);
+        bool ExchangeUdpPortsWithServer_CreateUdpSocks(int tcp_fd);
+    
+    private:
+        UdpInfo server_info_;
+};
+
+Client::Client()
+{
+
+}
+
+Client::~Client()
+{
+    close(server_info_.recv_fd);
+    close(server_info_.send_fd);
+}
+
+/*
+    Establishes tcp connection with server.
+    Returns false if something goes wrong.
+*/
+bool Client::CreateTcpConnectionWithServer(int *tcp_fd)
+{
+    *tcp_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (*tcp_fd == -1)
+    {
+        print_sock_err("Could not create tcp socket: ");
+        return false;
     }
 
-    return 0;
+    struct sockaddr_in servaddr = {};
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(SERVER_LISTEN_PORT);
+
+    if (connect(*tcp_fd, (const sockaddr*) &servaddr, sizeof(servaddr)) == -1)
+    {
+        print_sock_err("Could not connect to server: ");
+        return false;
+    }
+    printf("Connected to server\n");
+
+    return true;
+}
+
+/*
+    Exchange udp port numbers with the server using the initial tcp
+    socket and create udp sockets. Returns false if something goes wrong.
+*/
+bool Client::ExchangeUdpPortsWithServer_CreateUdpSocks(int tcp_fd)
+{
+    uint16_t send_port;
+    recv(tcp_fd, &send_port, 2, 0);
+    printf("Will be sending movement to client at port %u\n", send_port);
+
+    int udp_send_fd;
+    udp_send_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_send_fd == -1)
+    {
+        print_sock_err("Could not create udp send socket: ");
+        return false;
+    }
+
+    struct sockaddr_in servaddr = {};
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(send_port);
+
+    int udp_recv_fd;
+    udp_recv_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_recv_fd == -1)
+    {
+        print_sock_err("Could not create udp recv socket: ");
+        return false;
+    }
+
+    struct sockaddr_in cliaddr = {};
+    cliaddr.sin_family = AF_INET; 
+    cliaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    cliaddr.sin_port = 0;
+
+    if (bind(udp_recv_fd, (const struct sockaddr*) &cliaddr, sizeof(cliaddr)) != 0)
+    {
+        print_sock_err("Failed to bind udp port: ");
+        close(udp_recv_fd);
+        return false;
+    }
+
+    socklen_t addr_len = sizeof(cliaddr);
+    getsockname(udp_recv_fd, (struct sockaddr*)&cliaddr, &addr_len);
+    uint16_t recv_port = ntohs(cliaddr.sin_port);
+    printf("Game state will be received at port %u\n", recv_port);
+
+    // MAYBE DO A CHECK HERE ?
+    send(tcp_fd, &recv_port, 2, 0);
+
+    close(tcp_fd);
+
+    server_info_.recv_fd = udp_recv_fd;
+    server_info_.recv_port = recv_port;
+    server_info_.cliaddr = cliaddr;
+
+    server_info_.send_fd = udp_send_fd;
+    server_info_.send_port = send_port;
+    server_info_.servaddr = servaddr;
+
+    return true;
+}
+
+int main()
+{
+    Client client;
+
+    int tcp_fd;
+    client.CreateTcpConnectionWithServer(&tcp_fd);
+    client.ExchangeUdpPortsWithServer_CreateUdpSocks(tcp_fd);
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,216 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <thread>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define SERVER_LISTEN_PORT 8080
+#define print_sock_err(str)   perror(str); perror(strerror(errno)); perror("\n");
+#define CLIENT1 1
+#define CLIENT2 2
+
+// TODO: add contructor inside
+typedef struct
+{
+    int recv_fd;
+    uint16_t recv_port;
+    struct sockaddr_in servaddr;
+
+    int send_fd;
+    uint16_t send_port;
+    struct sockaddr_in cliaddr;
+
+} UdpInfo;
+
+
+class Server
+{
+    public:
+        Server(uint16_t tcp_port);
+        ~Server();
+        bool CreateTcpConnectionsWithClients(int *tcp_fd_cl1, int *tcp_fd_cl2);
+        bool ExchangeUdpPortsWithClient_CreateUdpSocks(int tcp_fd, int client_no);
+    
+    private:
+        uint16_t tcp_port_;
+        UdpInfo client1_info_;
+        UdpInfo client2_info_;
+};
+
+Server::Server(uint16_t tcp_port)
+{
+    tcp_port_ = tcp_port;
+
+    client1_info_.recv_fd = -1;
+    client1_info_.send_fd = -1;
+    client1_info_.recv_port = 0;
+    client1_info_.send_port = 0;
+
+    client2_info_.recv_fd = -1;
+    client2_info_.send_fd = -1;
+    client2_info_.recv_port = 0;
+    client2_info_.send_port = 0;
+}
+
+Server::~Server()
+{
+    close(client1_info_.recv_fd);
+    close(client1_info_.send_fd);
+    close(client2_info_.recv_fd);
+    close(client2_info_.send_fd);
+}
+
+/*
+    Establishes 2 tcp connections, one for each client and returns the fds
+    inside tcp_fd_cl1 and tcp_fd_cl2. Returns false if something goes wrong.
+*/
+bool Server::CreateTcpConnectionsWithClients(int *tcp_fd_cl1, int *tcp_fd_cl2)
+{
+    int listen_fd;
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd == -1)
+    {
+        print_sock_err("Could not create listen socket: ");
+        return false;
+    }
+
+    struct sockaddr_in servaddr = {};
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(tcp_port_); 
+
+    if (bind(listen_fd, (const struct sockaddr*) &servaddr, sizeof(servaddr)) != 0)
+    {
+        print_sock_err("Failed to bind listen port: ");
+        close(listen_fd);
+        return false;
+    }
+
+    if (listen(listen_fd, 1) != 0)
+    {
+        print_sock_err("Failed to listen: ");
+        close(listen_fd);
+        return false;
+    }
+
+    printf("Waiting for clients to connect...\n");
+
+    struct sockaddr_in cliaddr = {};
+    socklen_t cliaddr_len;
+    *tcp_fd_cl1 = accept(listen_fd, (struct sockaddr*) &cliaddr, &cliaddr_len);
+    if (*tcp_fd_cl1 == -1)
+    {
+        print_sock_err("Failed to accept first connection: ");
+        close(listen_fd);
+        return false;
+    }
+    printf("First client connected\n");
+
+    struct sockaddr_in cliaddr2 = {};
+    socklen_t cliaddr2_len;
+    *tcp_fd_cl2 = accept(listen_fd, (struct sockaddr*) &cliaddr2, &cliaddr2_len);
+    if (*tcp_fd_cl2 == -1)
+    {
+        print_sock_err("Failed to accept second connection: ");
+        close(listen_fd);
+        return false;
+    }
+    printf("Second client connected\n");
+
+    close(listen_fd);
+    return true;
+}
+
+/*
+    Exchange udp port numbers with the client using the initial tcp
+    socket and create udp sockets. Returns false if something goes wrong.
+*/
+bool Server::ExchangeUdpPortsWithClient_CreateUdpSocks(int tcp_fd, int client_no)
+{
+    int udp_recv_fd;
+    udp_recv_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_recv_fd == -1)
+    {
+        print_sock_err("Could not create udp recv socket: ");
+        return false;
+    }
+
+    struct sockaddr_in servaddr = {};
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = 0;
+
+    if (bind(udp_recv_fd, (const struct sockaddr*) &servaddr, sizeof(servaddr)) != 0)
+    {
+        print_sock_err("Failed to bind udp port: ");
+        close(udp_recv_fd);
+        return false;
+    }
+
+    socklen_t addr_len = sizeof(servaddr);
+    getsockname(udp_recv_fd, (struct sockaddr*)&servaddr, &addr_len);
+    uint16_t recv_port = ntohs(servaddr.sin_port);
+    printf("For client %d, movement will be received at port %u\n", client_no, recv_port);
+
+    // MAYBE DO A CHECK HERE ?
+    send(tcp_fd, &recv_port, 2, 0);
+
+    uint16_t send_port;
+    recv(tcp_fd, &send_port, 2, 0);
+    printf("For client %d, game state will be sent at port %u\n", client_no, send_port);
+
+    close(tcp_fd);
+
+    int udp_send_fd;
+    udp_send_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_send_fd == -1)
+    {
+        print_sock_err("Could not create udp send socket: ");
+        return false;
+    }
+
+    struct sockaddr_in cliaddr = {};
+    cliaddr.sin_family = AF_INET; 
+    cliaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    cliaddr.sin_port = htons(send_port); 
+
+    if (client_no == CLIENT1)
+    {
+        client1_info_.recv_fd = udp_recv_fd;
+        client1_info_.recv_port = recv_port;
+        client1_info_.servaddr = servaddr;
+
+        client1_info_.send_fd = udp_send_fd;
+        client1_info_.send_port = send_port;
+        client1_info_.cliaddr = cliaddr;
+    }
+    else    /* CLIENT2 */
+    {
+        client2_info_.recv_fd = udp_recv_fd;
+        client2_info_.recv_port = recv_port;
+        client2_info_.servaddr = servaddr;
+
+        client2_info_.send_fd = udp_send_fd;
+        client2_info_.send_port = send_port;
+        client2_info_.cliaddr = cliaddr;
+    }
+
+    return true;
+}
 
 int main()
 {
-    printf("It works!!!\n");
+    Server server(SERVER_LISTEN_PORT);
+    
+    int tcp_fd_cl1, tcp_fd_cl2;
+    server.CreateTcpConnectionsWithClients(&tcp_fd_cl1, &tcp_fd_cl2);
+    server.ExchangeUdpPortsWithClient_CreateUdpSocks(tcp_fd_cl1, CLIENT1);
+    server.ExchangeUdpPortsWithClient_CreateUdpSocks(tcp_fd_cl2, CLIENT2);
 }
